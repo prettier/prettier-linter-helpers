7fc3817fead6c1f7f5a8f0941739fc76260afd15
Ben Scott <ben@reload.me.uk>
2025-12-28 12:17:55 -0800

    linter fixups

--- eslint.config.js
+++ eslint.config.js
@@ -1,15 +1,14 @@
 const {defineConfig} = require('eslint/config');
-
 const js = require('@eslint/js');
-const node = require('eslint-plugin-n');
-const prettier = require('eslint-plugin-prettier/recommended');
+const eslintPluginN = require('eslint-plugin-n');
+const eslintPluginPrettierRecommended = require('eslint-plugin-prettier/recommended');
 
 module.exports = defineConfig([
   {
-    plugins: {js, n: node},
+    plugins: {js, n: eslintPluginN},
     extends: ['js/recommended', 'n/flat/recommended-script'],
   },
-  prettier,
+  eslintPluginPrettierRecommended,
   // Internal files
   {
     files: ['test/**', 'eslint.config.js'],
